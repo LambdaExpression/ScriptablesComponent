916606f4f7f19b04ea396243508a1f9cbc3542bb
LambdaExpression <13425088053@163.com>
2022-05-13 10:28:58 +0800

    Add files via upload

--- ChinaTelecomPanel/ChinaTelecomPanel.js
+++ ChinaTelecomPanel/ChinaTelecomPanel.js
@@ -1,11 +1,10 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: orange; icon-glyph: comments;
+// icon-color: blue; icon-glyph: mobile-alt;
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: code-branch;
 // 
-// name: ChinaTelecomPanel.js
 // https://github.com/LambdaExpression/Scriptables
 // 
 
